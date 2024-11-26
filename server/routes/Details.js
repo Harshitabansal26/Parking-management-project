a58b8831d691e88a6d8f1d7e1ca08be5f7dcb0e6
Harshita <harshitabansal3142@gmail.com>
2024-11-26 13:58:15 +0530

    updatation

--- server/routes/Details.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const { registerVehicle } = require("../controllers/DetailsController");
-
-const router = express.Router();
-
-router.post("/register", registerVehicle);
-
-module.exports = router;
--- server/routes/newsletterRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getNewsletter,createNewsletter} = require("../controllers/newsletterController");
-
-const{jwtAuthMiddleware} = require("../middleware/jwtMiddleware");
-
-router.get("/",getNewsletter);
-router.post("/",jwtAuthMiddleware,createNewsletter);
